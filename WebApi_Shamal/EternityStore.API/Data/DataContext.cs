75fde7effbcabbb37817ce168883fbca1aa406c3
Shamal De Saram <shamdzsar@gmail.com>
2020-06-18 08:08:56 +0530

    Implementation-Inital Commit

---
+++ WebApi_Shamal/EternityStore.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using EternityStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EternityStore.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebApi_Shamal/EternityStore.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace EternityStore.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
