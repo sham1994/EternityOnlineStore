e557aa23b55b13d68b68c75691e964f93f4a2427
Shamal De Saram <shamdzsar@gmail.com>
2020-06-22 23:59:23 +0530

    Updated Authentication

--- WebApi_Shamal/EternityStore.API/Startup.cs
+++ WebApi_Shamal/EternityStore.API/Startup.cs
@@ -12,6 +12,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace EternityStore.API
 {
@@ -20,6 +23,7 @@ namespace EternityStore.API
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            
         }
 
         public IConfiguration Configuration { get; }
@@ -31,13 +35,25 @@ namespace EternityStore.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))); 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            services.AddScoped<IAuthRepository,AuthRepository>();
             //3 services
             //1.AddSingleton = we create a single instance of our repository thorug out the application and it creates  the repository
             //for the first time and it reuses the object in all of the course. Can cause issues when it comes to concurrent requests
             //2.AddTransient = useful for light weight services, they are created each time they are requested
             //3.AddScoped = services is created once per request with in the scope
-
-            services.AddScoped<IAuthRepository,AuthRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                    .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +72,7 @@ namespace EternityStore.API
 
             //app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
